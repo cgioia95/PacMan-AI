140cda44bb9726f0624ffdc9efeab5b6162159d8
Christian Gioia <christiang101@gmail.com>
2019-08-21 15:32:44 +1000

    Part 1 and 2 Finalized, heuristic finished

--- comp90054-a1-2019-master/search/search.py
+++ comp90054-a1-2019-master/search/search.py
@@ -93,22 +93,7 @@ def depthFirstSearch(problem):
 
 
 
-
-
-def iterativeDeepeningSearch(problem):
-    """Search the deepest node in an iterative manner."""
-    "*** YOUR CODE HERE FOR TASK 1 ***"
-
-    "Intiialize the stack and set the initial Depth Limit to 1"
-    depthLimit = 1;
-    stack = util.Stack();
-
-    while True:
-
-        "Run this section every time we increment our depth to set up the Depth Limiting Search"
-        "Initializes visited states to null, pushes the starting node to stack"
-        "and adds the starting state to the visited states list "
-        "also accesses the key data from the node"
+def DepthLimitingSearch(stack, depthLimit, problem):
 
         visitedStates = []
         stack.push((problem.getStartState(), [], 0))
@@ -120,10 +105,6 @@ def iterativeDeepeningSearch(problem):
 
         visitedStates.append((state))
 
-        "This while loop runs the Depth Limiting Search by implementing the stack to search for the deepest node"
-        "Every time it runs, it checks if currently inspected state is the goal state"
-        "If the goal state, while loop breaks and skips to final check that returns the directions"
-        "Otherwise, it adds all the successors to the stack and inspects them too"
         while (problem.isGoalState(state) != True):
 
             succesorNodes = problem.getSuccessors(state)
@@ -150,17 +131,30 @@ def iterativeDeepeningSearch(problem):
             "If the stack isnt empty, pop it to inspect if has a goal state when the while performs its isGoalState check"
             "Otherwise break the goal state test as there are no more nodes to inspect at this depthLimit, so increment depthLimit"
             if (stack.isEmpty()):
-                break;
+                break
             else:
                 node = stack.pop()
                 state = node[0]
                 directionsToThisNode = node[1]
                 costToThisNode = node[2]
-        "final check to see if state is goal state, if so, returns its associated set of directions for Pacman"
+
         if (problem.isGoalState(state)):
             return directionsToThisNode
+        else:
+            return []
 
-        depthLimit += 1
+
+
+def iterativeDeepeningSearch(problem):
+    """Search the deepest node in an iterative manner."""
+
+    depthLimit = 1;
+    stack = util.Stack();
+
+    while (DepthLimitingSearch(stack, depthLimit, problem) == []):
+        depthLimit = depthLimit + 1
+
+    return DepthLimitingSearch(stack, depthLimit, problem)(stack, depthLimit, problem)
 
 
 def breadthFirstSearch(problem):
